cb44c16401c4b78dae3f76b04586b4d9bc632824
vincent <davidpdeveloper@gmail.com>
2019-01-24 08:00:38 +0800

    1

--- test1.py
+++ test1.py
@@ -1,18 +1,44 @@
 from flask import Flask
 import subprocess
-from flask_cors import CORS, cross_origin
+from flask import json
+from flask_cors import CORS 
+
 app = Flask(__name__)
 
+cors = CORS(app)
 @app.route('/')
 def hello():
     # import subproces
-     CMD = "mono ./m3/m1.exe"
+     CMD = "mono ./m1.exe 1"
      output = subprocess.check_output(CMD,shell=True)
      lst = []
      for char in output:
          lst.append(char) 
-     print ''.join(lst).split('\n')
-     return "ASDSDA"
+     tst =  ''.join(lst).split('\n')
+     #return jsonify(
+      #  data = tst
+    #)
+     print tst
+     return str(tst)
+
+
+@app.route('/add/<email>')
+def addVisit(email):
+    # import subproces
+     CMD = "mono ./m1.exe "+email
+     output = subprocess.check_output(CMD,shell=True)
+     lst = []
+     for char in output:
+         lst.append(char) 
+     tst =  ''.join(lst).split('\n')
+     #return jsonify(
+      #  data = tst
+    #)
+     print tst
+     return str(tst)
+
+
+    
 
 if __name__=='__main__':
     app.run('0.0.0.0')
